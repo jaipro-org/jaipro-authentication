0992215a8880ba0857d41bb228cade1f91179133
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-04 00:23:41 -0500

    deploy configured

--- src/main/java/com/bindord/eureka/auth/EurekaAuthenticationApplication.java
+++ src/main/java/com/bindord/eureka/auth/EurekaAuthenticationApplication.java
@@ -1,30 +1,17 @@
 package com.bindord.eureka.auth;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class EurekaAuthenticationApplication implements CommandLineRunner {
-
-    @Value("${conf.server.variable.demo}")
-    private String demoVal;
+public class EurekaAuthenticationApplication {
 
     @Autowired
     private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaAuthenticationApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(environment.getActiveProfiles()[0]);
-        System.out.println(environment.getProperty("conf.server.variable.demo"));
-        System.out.println("conf.server.variable.demo: " + demoVal);
     }
 }
