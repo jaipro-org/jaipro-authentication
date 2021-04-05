0ef58e07e955687d4c9a5266fc5cd9d2834f6ec3
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-04 21:04:39 -0500

    deployment's resources for pod added

---
+++ src/main/java/com/bindord/eureka/auth/configuration/CustomWebFilter.java
@@ -0,0 +1,22 @@
+package com.bindord.eureka.auth.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomWebFilter implements WebFilter {
+
+    private static final Logger LOGGER = LogManager.getLogger(CustomWebFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        return webFilterChain.filter(serverWebExchange).doFinally(
+                (signalType) -> LOGGER.info(serverWebExchange.getRequest().getPath())
+        );
+    }
+}
