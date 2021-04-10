4aa925e9e3b85e34a3473f0692bfee88fbef38da
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-10 13:05:12 -0500

    pip

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -4,11 +4,13 @@ node {
     def GIT_MASTER_CREDENTIALS_ID   ='GITHUB_PPCC'
     def MASTER_FOLDER               ='master'
     // - BASE PATHS
-    def JOB_NAME = env.JOB_NAME
-    def JOB_FULLPATH_CON = '/var/jenkins_home/workspace/' + JOB_NAME
-    def JOB_FULLPATH = env.WORKSPACE
+    def JOB_NAME            = env.JOB_NAME
+    def JOB_FULLPATH_CON    = '/var/jenkins_home/workspace/' + JOB_NAME
+    def JOB_FULLPATH        = env.WORKSPACE
+    def BASE_CONFIGMAP      = 'config-map-base.yaml'
 
     // SERVICE PROPS
+    def PRODUCT_NAME    = 'hogarep'
     def SVC_NAME        = 'eureka-authentication'
     def SVC_FOLDER      = 'service'
 
@@ -18,14 +20,17 @@ node {
         sh "echo '******INITIALIZING.....'"
     }
 
-    /*stage('FETCHING SERVICE PROPERTIES') {
+    stage('FETCHING SERVICE PROPERTIES') {
         dir(MASTER_FOLDER) {
             sh "echo '****** STARTING PHASE: fetching service properties'"
             git branch: 'main', credentialsId: GIT_MASTER_CREDENTIALS_ID, url: 'https://github.com/bindord-org/master-properties.git'
         }
+
+        sh "sed -ie \"s/\\SVC_NAME/$SVC_NAME/\" $BASE_CONFIGMAP"
+        sh "cat $BASE_CONFIGMAP"
     }
 
-    stage('INIT2') {
+    /*stage('INIT2') {
         dir(SVC_FOLDER) {
             sh "echo '****** STARTING PHASE: init'"
             git branch: 'main', credentialsId: GIT_MASTER_CREDENTIALS_ID, url: "https://github.com/bindord-org/$SVC_NAME"+".git"
