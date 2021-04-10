e83fc94f9f8f5373918c5298288fe1921c488f81
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-10 13:12:22 -0500

    pip

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -27,8 +27,9 @@ node {
 
         }
 
-        sh "sed -ie \"s/\\SVC_NAME/$SVC_NAME/\" $BASE_CONFIGMAP"
-        sh "cat $BASE_CONFIGMAP"
+        sh "ls"
+//        sh "sed -ie \"s/\\SVC_NAME/$SVC_NAME/\" $BASE_CONFIGMAP"
+//        sh "cat $BASE_CONFIGMAP"
     }
 
     /*stage('INIT2') {
