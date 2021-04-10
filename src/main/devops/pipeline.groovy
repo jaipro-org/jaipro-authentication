73a5461d9c14edd327d277a3641c777d40178119
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-09 23:07:42 -0500

    pip edit

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -1,7 +1,24 @@
 node {
 
-    stage('INIT') {
-        sh "echo '******INITIALIZING.....'"
+    // FUNDAMENTAL_PROPS
+    def GIT_MASTER_CREDENTIALS_ID   ='GITHUB_PPCC'
+    def MASTER_FOLDER               ='master'
 
+    // SERVICE PROPS
+    def SVC_NAME        = 'eureka-authentication'
+    def SVC_FOLDER      = 'SERVICE'
+
+
+    stage('FETCHING SERVICE PROPERTIES') {
+        dir(MASTER_FOLDER) {
+            git branch: 'main', credentialsId: GIT_MASTER_CREDENTIALS_ID, url: 'https://github.com/bindord-org/master-properties.git'
+        }
     }
+
+    stage('INIT ') {
+        dir(SVC_FOLDER) {
+            git branch: 'main', credentialsId: $GIT_MASTER_CREDENTIALS_ID, url: 'https://github.com/bindord-org/$SVC_NAME.git'
+        }
+    }
+
 }
