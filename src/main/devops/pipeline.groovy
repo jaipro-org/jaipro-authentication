ba4ae0326c99b5eaeb5372e2f08fd54b326d9d37
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-10 14:05:30 -0500

    pip

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -39,9 +39,8 @@ node {
 
     stage('DEPLOYING TO K8S') {
         sh 'echo "INIT K8S...."'
-        sh 'ls'
         //acsDeploy azureCredentialsId: 'AKS_BINDORD_CLIENT', configFilePaths: 'app/src/main/devops/deployment.yml', containerService: 'cluster-webapps | AKS', dcosDockerCredentialsPath: '', resourceGroupName: 'rg-devops', secretName: '', sshCredentialsId: '96a4e79f-641b-4521-9a35-d41bf54224e8'
-        kubernetesDeploy configs: "master/base/**.yaml", kubeconfigId: K8S_LOCAL, enableConfigSubstitution: true
+        kubernetesDeploy configs: "$MASTER_FOLDER/$BASE_CONFIGMAP", kubeconfigId: K8S_LOCAL, enableConfigSubstitution: true
     }
 
     /*stage('INIT2') {
