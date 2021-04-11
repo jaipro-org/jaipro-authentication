316ae52100577af6a71278e471d43263965fb423
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-11 14:48:50 -0500

    pip

--- src/main/devops/pipeline.groovy
+++ src/main/devops/pipeline.groovy
@@ -81,10 +81,11 @@ node {
                 "maven:3.8.1-openjdk-11-slim " +
                 "mvn clean package"
 
-        def SVC_VERSION = sh "cat $SVC_FOLDER/pom.xml " +
-                                '| grep -B 1 \'name\' ' +
-                                '| grep \'<version>\' ' +
-                                '| sed -e \'s/^[[:space:]]*//\' | cut -c 10- | rev | cut -c 11- | rev'
+        def SVC_VERSION = sh script: "cat $SVC_FOLDER/pom.xml " +
+                            '| grep -B 1 \'name\' ' +
+                            '| grep \'<version>\' ' +
+                            '| sed -e \'s/^[[:space:]]*//\' | cut -c 10- | rev | cut -c 11- | rev',
+                            returnStdout: true
 
         sh "docker build " +
                 "-t $CR_BINDORD_HOST/$SVC_NAME:${SVC_VERSION} " +
