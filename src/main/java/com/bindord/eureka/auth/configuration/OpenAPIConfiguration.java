b6383b6ddf33cdf561a9bb4a6a50212bef149283
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-13 18:39:39 -0500

    openapi bean added

---
+++ src/main/java/com/bindord/eureka/auth/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,23 @@
+package com.bindord.eureka.auth.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("Eureka Authentication API").version(appVersion)
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
