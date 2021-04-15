69641cae601d59794fa7b4850b362c6b642040fa
peterzinho16 <peterpaul.0194@gmail.com>
2021-04-15 00:02:12 -0500

    jwt verify with default config related to jwt-issuer-uri

--- src/main/java/com/bindord/eureka/auth/configuration/SecurityConfiguration.java
+++ src/main/java/com/bindord/eureka/auth/configuration/SecurityConfiguration.java
@@ -1,9 +1,9 @@
 package com.bindord.eureka.auth.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -15,16 +15,20 @@ public class SecurityConfiguration {
     @Value("${service.ingress.context-path}")
     private String svcContextPath;
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String jwtIssuerURI;
+
+
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        //svcContextPath + "/**"
         http
                 .authorizeExchange()
                 .pathMatchers("/actuator/**").permitAll()
                 .anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt(Customizer.withDefaults());
         return http.build();
 
     }
